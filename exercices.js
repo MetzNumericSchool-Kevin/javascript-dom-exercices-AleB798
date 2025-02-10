ef6fbbe629741cc140ca8360bc570a72fa5f3219
Alexandra Baumann <alexbaumann342@gmail.com>
2025-02-10 09:42:19 +0100

    Des informations manquent !

--- exercices.js
+++ exercices.js
@@ -2,3 +2,14 @@
 
 let nameShop = document.querySelector('h1').textContent;
 console.log(nameShop);
+
+// ===== Des informations manquent ! ===== 
+
+// On cible le parent qui contiendra le nouveau paragraphe
+let shopDescription = document.querySelector('#description_boutique')
+// On crée un nouvel élément P
+let newElement = document.createElement("p")
+// On précise le texte qui sera contenu dans le p grâce à textContent
+newElement.textContent = 'Pour cela, il vous suffit de cliquer sur une des potions de la liste ci-dessous pour l\'acheter.'
+// On utilise la méthode appenchild() pour ajouter le nouvel élément à son parent en passant en paramètre le nouvel élément
+shopDescription.appendChild(newElement)
